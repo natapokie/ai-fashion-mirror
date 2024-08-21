ab446c4ff392ce7eb464ef3461780716fbe0eda6
Natalie Chan <natapoke2@gmail.com>
2024-08-20 22:23:46 -0400

    removing TODO comment

--- client/pages/socket.tsx
+++ client/pages/socket.tsx
@@ -1,6 +1,5 @@
 import { io } from 'socket.io-client';
 
-// TODO replace with an env variable
 export const socket = io(`${process.env.SERVER_BASE_URL}`, {
   transports: ['websocket', 'polling'],
 });
