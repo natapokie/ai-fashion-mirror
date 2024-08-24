b0365eb1178ca07fd00614104339a4d59a8caf69
Natalie Chan <natapoke2@gmail.com>
2024-08-24 17:51:07 -0400

    fix eslintno-prototype-builtins -- removing hasOwnProperty()

---
+++ server/src/utils/parseResponse.ts
@@ -0,0 +1,40 @@
+import { ResponseData, SocialMediaComments } from '../../../shared/types';
+import { WORDS_PER_MS, MIN_DISPLAY_TIME } from './constants';
+
+const calculateDisplayTime = (text: string): number => {
+  // Ensure text is a string and not empty or undefined
+  if (typeof text !== 'string' || text.trim() === '') {
+    console.error('Invalid or empty text provided');
+    return MIN_DISPLAY_TIME; // Return the minimum display time if input is invalid
+  }
+
+  const words = text.split(' ').length;
+  const millisecToRead = Math.round(words / WORDS_PER_MS);
+
+  return Math.max(millisecToRead, MIN_DISPLAY_TIME);
+};
+
+export const parseResponse = (jsonResponse: SocialMediaComments): ResponseData => {
+  const responseData: ResponseData = {
+    comments: [],
+    likes: 0,
+    views: 0,
+    commentsCount: 0,
+  };
+
+  for (const key in jsonResponse) {
+    if (key === 'likes') {
+      responseData.likes = jsonResponse.likes;
+    } else if (key === 'views') {
+      responseData.views = jsonResponse.views;
+    } else if (key === 'comments') {
+      responseData.commentsCount = jsonResponse.comments;
+    } else {
+      const [user, text] = jsonResponse[key].split(': ');
+      const displayTime = calculateDisplayTime(text);
+      responseData.comments.push({ user, text, displayTime });
+    }
+  }
+
+  return responseData;
+};
