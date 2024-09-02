a62c9ad952ed33877bd585a74c4520f24dd27417
Eri Yoshida <74118903+eriysd@users.noreply.github.com>
2024-09-02 09:53:06 -0400

    bigger

--- client/components/spinner/spinner.tsx
+++ client/components/spinner/spinner.tsx
@@ -2,7 +2,7 @@ export const Spinner = () => {
   // TODO: adjust size of spinner
   return (
     <div
-      className="inline-block h-20 w-20 animate-spin rounded-full border-8 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+      className="inline-block h-40 w-40 animate-spin rounded-full border-20 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
       role="status"
     >
       <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -12,6 +12,12 @@ const config: Config = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      fontSize: {
+        '10xl': ['12rem', { lineHeight: '1.5' }], 
+      },
+      borderWidth: {
+        '20': '20px',
+      },
     },
   },
   plugins: [],
