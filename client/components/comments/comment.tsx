5550f230eea014a15895981a10145b6375b30c69
Eri Yoshida <74118903+eriysd@users.noreply.github.com>
2024-09-03 16:34:07 -0400

    changing the comment animation

--- client/components/comments/comment.tsx
+++ client/components/comments/comment.tsx
@@ -11,9 +11,9 @@ const Comment: React.FC<CommentProps> = ({ comment }) => {
   return (
     <motion.div
       className="bg-slate-950/70 text-white p-3 rounded-t-2xl rounded-r-2xl shadow-md mb-2 absolute bottom-0"
-      initial={{ opacity: 0, y: 50 }}
-      animate={{ opacity: 1, y: 0, transition: { duration: 3, delay:2 } }}
-      exit={{ opacity: 0, y: -600, transition: { duration: 10, delay:1 } }}
+      initial={{ opacity: 0, y: -550 }}
+      animate={{ opacity: 1, y: -500, transition: { duration: 3, delay:2 } }}
+      exit={{ opacity: 0, y: 0, transition: { duration: 20, delay:1 } }}
     >
       <>
         <h3>
