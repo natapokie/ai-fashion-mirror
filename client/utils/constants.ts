a4735d9fb4e9c93c43caac717a6dcd4ff1589739
vicliu2001 <116909592+vicliu2001@users.noreply.github.com>
2024-09-03 20:52:11 -0400

    added more constants

--- client/utils/constants.ts
+++ client/utils/constants.ts
@@ -1,2 +1,7 @@
 export const LIKES_INCREMENT_STEP = 8; // # of increment when displayed likes is less than finalLikes
 export const LIKES_INCREMENT_DELAY = 15; // displayed likes increases by LIKES_INCREMENT_STEP every LIKES_INCREMENT_DELAY millisecs
+export const LIKES_ANIMATION_DELAY = 4000; // delay in millisec the likes animation to wait for comments to start rolling
+export const ANIMATION_DURATION_LOWER = 1000; // lower limit for animation runtime
+export const ANIMATION_DURATION_UPPER = 2000; // upper limit for animation runtime
+export const PAUSE_DURATION_LOWER = 2000; // lower limit for pause duration
+export const PAUSE_DURATION_UPPER = 3000; // upper limit for pause duration
