b47e8dd1a23e689bc289fcbd0e8404ce2df71441
Natalie Chan <natapoke2@gmail.com>
2024-09-09 15:26:26 -0400

    making photo thumbnail smaller

--- client/components/photo/photo.tsx
+++ client/components/photo/photo.tsx
@@ -3,7 +3,7 @@ export const Photo = ({ img }: { img: string }) => {
 
   return (
     <div className="fixed top-4 right-4 border border-gray-300 shadow-md">
-      <img id="image" alt="Received image" src={img} width={400} height={400}/>
+      <img id="image" alt="Received image" src={img} width={100} />
     </div>
   );
 };
