138b2b53aaf14962c04759ce71bae943485cee85
Natalie Chan <natapoke2@gmail.com>
2024-10-29 18:57:38 -0400

    adding mockdata for testing

---
+++ client/utils/mockData.ts
@@ -0,0 +1,74 @@
+import { ProductData } from '../../shared/types';
+
+export const mockProductData: ProductData[] = [
+  {
+    image:
+      'https://images.canadagoose.com/image/upload/w_1844,c_scale,f_auto,q_auto/v1726560988/product-image/2051M_66_fsph.jpg',
+    name: 'Expedition Parka',
+    feedback:
+      'An iconic parka designed for extreme weather, perfect for those who face harsh winter conditions.',
+  },
+  {
+    image:
+      'https://images.canadagoose.com/image/upload/w_1844,c_scale,f_auto,q_auto/v1700417215/product-image/2053M_61_fsph.jpg',
+    name: 'Chateau Jacket',
+    feedback:
+      'Combines warmth and style, making it ideal for urban settings where both fashion and function are a must.',
+  },
+  {
+    image:
+      'https://images.canadagoose.com/image/upload/w_614,c_scale,f_auto,q_auto/v1706552315/product-image/9501L_11.jpg',
+    name: 'Snow Mantra Parka',
+    feedback:
+      'Engineered for the coldest environments, it/s ideal for extreme outdoor activities and expeditions.',
+  },
+  {
+    image:
+      'https://images.canadagoose.com/image/upload/w_359,c_scale,f_auto,q_auto/v1726648199/product-image/2714M_63_a.jpg',
+    name: 'HyBridge Lite Jacket',
+    feedback:
+      "A lightweight, packable jacket that's perfect for layering or wearing on mild winter days.",
+  },
+  {
+    image:
+      'https://images.canadagoose.com/image/upload/w_614,c_scale,f_auto,q_auto/v1726561366/product-image/2057MW_433.jpg',
+    name: 'Paradigm Freestyle Vest',
+    feedback:
+      "A versatile vest for added core warmth that's easy to layer and great for transitional weather.",
+  },
+  {
+    image:
+      'https://images.canadagoose.com/image/upload/w_614,c_scale,f_auto,q_auto/v1726606811/product-image/6660W_432.jpg',
+    name: 'Trillium Parka',
+    feedback:
+      'Designed for everyday winter wear, offering comfort, warmth, and a relaxed silhouette.',
+  },
+  {
+    image:
+      'https://images.canadagoose.com/image/upload/w_614,c_scale,f_auto,q_auto/v1721660263/product-image/2080M_432.jpg',
+    name: 'Macmillan Parka',
+    feedback:
+      'With a sleek design, this parka is perfect for city life, blending style with reliable warmth.',
+  },
+  {
+    image:
+      'https://images.canadagoose.com/image/upload/w_614,c_scale,f_auto,q_auto/v1726406860/product-image/4151M1_1551.jpg',
+    name: 'Garson Vest Wool',
+    feedback:
+      "Lightweight and warm, it's ideal for layering under a coat or over a sweater in colder months.",
+  },
+  {
+    image:
+      'https://images.canadagoose.com/image/upload/w_614,c_scale,f_auto,q_auto/v1726577779/product-image/5078M_222.jpg',
+    name: 'Lodge Hoody',
+    feedback:
+      'Warm and windproof, this hoodie is ideal for both urban outings and active winter adventures.',
+  },
+  {
+    image:
+      'https://images.canadagoose.com/image/upload/w_614,c_scale,f_auto,q_auto/v1699469101/product-image/2580L_432.jpg',
+    name: 'Rossclair Parka Heritage',
+    feedback:
+      "This parka's slim fit and style make it perfect for anyone looking for a blend of fashion and function.",
+  },
+];
