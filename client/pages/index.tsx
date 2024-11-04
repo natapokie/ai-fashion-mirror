6ab3975bb1ff438a9a48a734219861dc74fcbafb
Natalie Chan <natapoke2@gmail.com>
2024-11-03 22:53:58 -0500

    adding some sliding animations when the carousel loads

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -2,6 +2,11 @@ import React, { useState } from 'react';
 import CameraFeed from '@/components/cameraFeed/cameraFeed';
 import Carousel from '@/components/carousel/carousel';
 import { mockProductData } from '@/utils/mockData';
+import Image from 'next/image';
+import { AnimatePresence } from 'framer-motion';
+import { motion } from 'framer-motion';
+
+const closeIcon = '/icons/xmark-solid.svg';
 
 const Home = () => {
   const [showCarousel, setShowCarousel] = useState(false);
@@ -10,7 +15,7 @@ const Home = () => {
     setShowCarousel(true);
   };
 
-  const handleClickOutside = () => {
+  const closeCarousel = () => {
     setShowCarousel(false); // Reset to the initial state
   };
 
@@ -34,11 +39,27 @@ const Home = () => {
         >
           CLICK TO START
         </button>
-      ) : (
-        <div className="w-full h-full flex flex-col justify-end items-center">
-          <Carousel products={mockProductData} onClickOutside={handleClickOutside} />
-        </div>
-      )}
+      ) : null}
+
+      <AnimatePresence>
+        {showCarousel && (
+          <motion.div
+            initial={{ x: '100%' }}
+            animate={{ x: 0 }}
+            exit={{ x: '100%' }}
+            transition={{ type: 'tween', ease: 'easeInOut', duration: 0.5 }}
+            className="w-full h-full flex flex-col justify-end items-center z-10"
+          >
+            <button
+              onClick={closeCarousel}
+              className="absolute top-0 right-0 opacity-80 hover:opacity-100 transition-opacity p-4 box-content z-10"
+            >
+              <Image src={closeIcon} alt="back" width={24} height={24} />
+            </button>
+            <Carousel products={mockProductData} onClickOutside={closeCarousel} />
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 };
