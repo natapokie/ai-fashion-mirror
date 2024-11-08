545d99106c2c503234e30b2548b480030ba298ff
viiiiiicccky <pj1483333@gmail.com>
2024-11-07 21:02:19 -0500

    updated server to use HTTPS with self-signed SSL certificate

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,5 @@
-import http from 'http';
+import https from 'https'; // import https, set up certificate (for windows as well)
+import fs from 'fs';
 import app from './app';
 import dotenv from 'dotenv';
 import path from 'path';
@@ -10,11 +11,18 @@ dotenv.config({
   path: path.resolve(__dirname, '../../.env'),
 });
 
-const server = http.createServer(app);
+// load certificate and key from environment variables
+const credentials = {
+  key: fs.readFileSync(path.resolve(__dirname, '../../', process.env.SSL_KEY_PATH!), 'utf8'),
+  cert: fs.readFileSync(path.resolve(__dirname, '../../', process.env.SSL_CERT_PATH!), 'utf8'),
+};
+
+const server = https.createServer(credentials, app);
 
 const io = new Server(server, {
   cors: {
     origin: process.env.CLIENT_BASE_URL,
+    credentials: true,
   },
   transports: ['websocket', 'polling'],
 });
@@ -23,5 +31,5 @@ const io = new Server(server, {
 new SocketManager(io);
 
 server.listen(process.env.SERVER_PORT || 8080, () => {
-  console.log(`Server started on port: ${process.env.SERVER_PORT}`);
+  console.log(`Server started on HTTPS port: ${process.env.SERVER_PORT}`);
 });
