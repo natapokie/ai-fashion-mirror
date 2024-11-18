58879a3a823f7150b12052e2d44ae50c972ad31d
Victor Liu <vicliu2001@gmail.com>
2024-11-18 13:51:15 -0500

    fixed types import

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -9,7 +9,7 @@ import { Countdown } from '@/components/countdown/countdown';
 import { Spinner } from '@/components/spinner/spinner';
 import { useCamera } from '@/context/cameraContext';
 import { saveImage } from '@/services/cameraService';
-import { ProductData } from '@/types';
+import { ProductData } from '../../shared/types';
 
 const closeIcon = '/icons/xmark-solid.svg';
 
