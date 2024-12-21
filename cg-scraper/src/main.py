427329f075b10836783adf90477bb200ca278379
Natalie Chan <natapoke2@gmail.com>
2024-12-21 01:22:52 -0500

    testing changes to precommit

--- cg-scraper/src/main.py
+++ cg-scraper/src/main.py
@@ -1,9 +1,13 @@
 from scraper import Scraper
 
+
 def main():
     scraper = Scraper()
     scraper.run()
 
+
 # Run the main coroutine
 if __name__ == "__main__":
     main()
+    print("testing")
+    hello = 0
