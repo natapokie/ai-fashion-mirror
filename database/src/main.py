5dcdd0be92f07842049006a88e600033cac2a8d4
Eri Yoshida <74118903+eriysd@users.noreply.github.com>
2025-02-03 18:01:52 -0500

    implementing sanitizer and db to clean the data and integrate pinecone

--- database/src/main.py
+++ database/src/main.py
@@ -1,10 +1,30 @@
+import sys
+import os
 from scraper import Scraper
-
+from sanitizer import Sanitizer
+from db import DatabaseHelper
 
 def main():
-    scraper = Scraper()
-    scraper.run()
-
+    # scrape data if "--s" flag is passed or if the output file does not exist
+    if "--scrape" in sys.argv or not os.path.exists("data/output.csv"): 
+        print("Scraping data")
+        scraper = Scraper()
+        scraper.run()
+    if "--sanitize" in sys.argv or not os.path.exists("data/cleaned_output.json"):
+        print("Sanitizing data")
+        sanitizer = Sanitizer()
+        sanitizer.run()
+    if "--upsert" in sys.argv:
+        print("Upserting data")
+        db = DatabaseHelper()
+        db.upsert_handler()
+    if "--describe" in sys.argv:
+        db = DatabaseHelper()
+        db.describe_index()
+    if "--query" in sys.argv:
+        db = DatabaseHelper()
+        query = "canada goose"
+        db.query_index(query)
 
 if __name__ == "__main__":
     main()
