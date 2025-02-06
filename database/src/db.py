c4fc535216896048570e24d132e9a0a4b25d0dc7
Eri Yoshida <74118903+eriysd@users.noreply.github.com>
2025-02-05 21:57:45 -0500

    removing test data

--- database/src/db.py
+++ database/src/db.py
@@ -4,24 +4,6 @@ import openai
 from pinecone import Pinecone
 from dotenv import load_dotenv
 
-# Define a sample dataset where each item has a unique ID and piece of text
-sampleData = [
-    {
-        "id": "vec1",
-        "text": "Apple is a popular fruit known for its sweetness and crisp texture.",
-    },
-    {
-        "id": "vec2",
-        "text": "The tech company Apple is known for its innovative products like the iPhone.",
-    },
-    {"id": "vec3", "text": "Many people enjoy eating apples as a healthy snack."},
-    {
-        "id": "vec4",
-        "text": "Apple Inc. has revolutionized the tech industry with its sleek designs and user-friendly interfaces.",
-    },
-    {"id": "vec5", "text": "An apple a day keeps the doctor away, as the saying goes."},
-]
-
 
 class DatabaseHelper:
     def __init__(self):
