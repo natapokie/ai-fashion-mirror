c343c0fde055c7b5c32c1026b4cef30de849295c
Natalie Chan <natapoke2@gmail.com>
2025-02-09 10:31:41 -0500

    swagger docs for pinecone api

--- server/src/routes/gptRoutes.ts
+++ server/src/routes/gptRoutes.ts
@@ -4,6 +4,32 @@ import { upload } from '../multer';
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * /gpt/ask:
+ *   post:
+ *     deprecated: true
+ *     tags:
+ *       - gpt
+ *     summary: Saves image to disk and queries OpenAI
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         multipart/form-data:
+ *           schema:
+ *             $ref: '#/components/schemas/FormDataRequestBody'
+ *     responses:
+ *       "200":
+ *         description: Successfully saved image to disk and queried OpenAI
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/SuccessResponse'
+ *       "400":
+ *         $ref: '#/components/responses/BadRequest'
+ *       "500":
+ *         $ref: '#/components/responses/ServerError'
+ */
 router.post('/ask', upload.single('image'), GptController.ask);
 
 export default router;
