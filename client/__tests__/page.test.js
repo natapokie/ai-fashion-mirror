c157a61b4dd9cf0b755127223548b0d16877d0cb
Natalie Chan <natapoke2@gmail.com>
2025-02-16 18:02:49 -0500

    very very basic test to make sure jest is working

---
+++ client/__tests__/page.test.js
@@ -0,0 +1,8 @@
+import '@testing-library/jest-dom';
+import { describe, expect, it } from '@jest/globals';
+
+describe('Page', () => {
+  it('test true', () => {
+    expect(true).toBe(true);
+  });
+});
