8d5161e65efa0fd0a86df8b64016d47768a5c240
Natalie Chan <natapoke2@gmail.com>
2025-02-16 18:42:09 -0500

    fixing test to test Home page

--- client/__tests__/page.test.js
+++
@@ -1,8 +0,0 @@
-import '@testing-library/jest-dom';
-import { describe, expect, it } from '@jest/globals';
-
-describe('Page', () => {
-  it('test true', () => {
-    expect(true).toBe(true);
-  });
-});
