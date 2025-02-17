d2a9c67be6a26bd8528f9488ec9392fda07a8721
Natalie Chan <natapoke2@gmail.com>
2025-02-17 10:09:23 -0500

    adding tests for scraper and sanitizer

---
+++ database/tests/test_sanitizer.py
@@ -0,0 +1,59 @@
+import pytest
+import pandas as pd
+from database.src.sanitizer import Sanitizer
+
+sanitizer = Sanitizer()
+
+
+def test_run():
+    """
+    Test that the data is cleaned and saved correctly.
+    """
+    pass
+
+
+@pytest.mark.parametrize(
+    "row, expected_result",
+    [
+        (pd.Series({"cimulateTags": "jacket, down, men"}), "jacket, down, men"),
+        (pd.Series({"colorName": "Black", "cimulateTags": None}), "black"),
+        (
+            pd.Series(
+                {
+                    "colorName": "Navy",
+                    "fabricTechnology": "Nylon-Tech-Shell",
+                    "cimulateTags": None,
+                }
+            ),
+            "navy, nylon-tech-shell",
+        ),
+    ],
+)
+def test_generate_embedding_tags(row, expected_result):
+    """
+    Test that the embedding tags are generated correctly.
+    """
+    assert sanitizer.generate_embedding_tags(row) == expected_result
+
+
+@pytest.mark.parametrize(
+    "value, expected_result",
+    [
+        ("['Nylon-Tech-Shell']", "Nylon-Tech-Shell"),
+        (
+            "['lightweight-down-jackets', 'hybridge-knits']",
+            "lightweight-down-jackets hybridge-knits",
+        ),
+        ("relaxed", "relaxed"),
+        # TODO: check this test case -- is this the expected result?
+        (
+            "[{'name': 'Colour', 'id': 'Color', 'swatchable': True, 'values': []}]",
+            "{'name': 'Colour', 'id': 'Color', 'swatchable': True, 'values': []}",
+        ),
+    ],
+)
+def test_clean_array_string(value, expected_result):
+    """
+    Test that array strings are cleaned correctly.
+    """
+    assert sanitizer.clean_array_string(value) == expected_result
--- database/tests/test_scraper.py
+++ database/tests/test_scraper.py
@@ -1,8 +1,6 @@
 import pytest
 import json
-from src.scraper import Scraper
-
-scraper = Scraper()
+from database.src.scraper import Scraper
 
 
 @pytest.fixture
@@ -37,6 +35,7 @@ def test_refresh_token():
     """
     Test that the access token is refreshed/updated.
     """
+    scraper = Scraper()
     scraper.refresh_tokens()
     assert scraper.access_token is not None
     assert scraper.expires_in is not None
@@ -46,7 +45,22 @@ def test_parse_image_cache(init_image_cache):
     """
     Test that the image cache is parsed correctly.
     """
+    scraper = Scraper()
     main_image, other_images = scraper.parse_image_cache(init_image_cache)
     assert main_image.endswith("1272MCD_9061.jpg")
     assert len(other_images) == 1
     assert any(item.endswith("1272MCD_9061_s.jpg") for item in other_images)
+
+
+def test_make_request():
+    """
+    Test that a request is made correct error message is logged.
+    """
+    pass
+
+
+def test_call_api():
+    """
+    Test that an API is called.
+    """
+    pass
