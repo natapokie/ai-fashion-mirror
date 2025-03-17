8932e7d5764c1577d018e598a48ad01ec1c5ac5d
Natalie Chan <natapoke2@gmail.com>
2025-03-17 01:16:13 -0400

    adjust next config

--- client/next.config.js
+++ client/next.config.js
@@ -1,11 +1,11 @@
-/* eslint-disable @typescript-eslint/no-require-imports */
+ 
 /** @type {import('next').NextConfig} */
-const dotenv = require('dotenv');
-const path = require('path');
+// const dotenv = require('dotenv');
+// const path = require('path');
 
-dotenv.config({
-  path: path.resolve(__dirname, '../.env'),
-});
+// dotenv.config({
+//   path: path.resolve(__dirname, '../.env'),
+// });
 
 const nextConfig = {
   reactStrictMode: true,
@@ -14,6 +14,7 @@ const nextConfig = {
     PREAPI_URL: process.env.PREAPI_URL,
     CLIENT_BASE_URL: process.env.CLIENT_BASE_URL,
     SERVER_BASE_URL: process.env.SERVER_BASE_URL,
+    NEXT_PUBLIC_SERVER_BASE_URL: process.env.NEXT_PUBLIC_SERVER_BASE_URL,
   },
 };
 
